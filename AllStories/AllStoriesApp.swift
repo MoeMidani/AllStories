85318bbfc7143f514411919e0fa69aabff209fc8
Moe <mohamedmidani2001@gmail.com>
2023-11-21 08:54:32 -0500

    Initial Commit

---
+++ AllStories/AllStoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllStoriesApp.swift
+//  AllStories
+//
+//  Created by Mohamed Midani on 11/21/23.
+//
+
+import SwiftUI
+
+@main
+struct AllStoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
